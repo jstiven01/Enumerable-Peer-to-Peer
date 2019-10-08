be305247ccdfcb1b18182670f12cd56b2e7740f4
jstiven01 <stiven10@gmail.com>
2019-10-07 19:24:12 -0500

    Creating my_each method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,3 +1,29 @@
 # frozen_string_literal: true
 
 # Project 2: Enumerable Methods
+module Enumerable
+    def my_each
+      # your code here
+      if self.is_a?(Array)
+        (self.length).times do |i|
+            yield (self[i])
+        end
+      elsif self.is_a?(Hash)
+        (self.keys.length).times do |i|
+          yield [self.keys[i], self.values[i]]
+        end
+      end
+    end
+end
+
+test = [1,343,3,4]
+#test = {
+#    jaws: "hey",
+#    gadfs: "hey1121",
+#    rrr: "hey1212100000"
+#}
+#p test.keys.length
+test.my_each {|x| p x}
+#test.my_each {|x, y| p x, y}
+
+
